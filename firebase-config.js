0125fc8137f909fe647649893eadc63ddeddda7f
aoPEN3024 <aoyama.paint5022@gmail.com>
2026-06-30 16:18:02 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase接続設定
+// Firebaseコンソール → プロジェクトの設定 → マイアプリ（ウェブ）の値に差し替えてください。
+// admin.html と ky.html の両方がこのファイルを読み込みます。
+export const firebaseConfig = {
+  apiKey: "AIzaSyCu81Ual13ACiFEePpOZ5qKb9k6mbv_dQ0",
+  authDomain: "aoky-31987.firebaseapp.com",
+  projectId: "aoky-31987",
+  storageBucket: "aoky-31987.firebasestorage.app",
+  messagingSenderId: "412721736846",
+  appId: "1:412721736846:web:da7259cc7734b3868feddf",
+  measurementId: "G-632C81MTZY"
+};
